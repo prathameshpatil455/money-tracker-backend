a0faea8914b568a4f264fea45424ff002b5ce5b9
prathamesh patil <132359689+prathameshpatil455@users.noreply.github.com>
2025-06-14 19:03:54 +0530

    changes in serverjs

--- src/server.js
+++ src/server.js
@@ -13,7 +13,7 @@ import transactionRoutes from "./routes/transactionRoutes.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 // middleware
@@ -40,7 +40,7 @@ if (process.env.NODE_ENV === "production") {
 }
 
 connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log("Server started on PORT:", PORT);
+  app.listen(port, () => {
+    console.log("Server started on PORT:", port);
   });
 });
